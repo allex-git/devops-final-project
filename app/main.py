aeedab07ac83390c7a0ce470869b72d24511b32c
allex-git <allexshop365@gmail.com>
2026-04-22 18:43:51 +0300

    main.py_new_data

--- app/main.py
+++ app/main.py
@@ -1,25 +1,49 @@
-from fastapi import FastAPI
-import socket
+# from fastapi import FastAPI
+# import socket
 
-app = FastAPI()
+# app = FastAPI()
 
-def get_host_info():
-    hostname = socket.gethostname()
-    try:
-        ip = socket.gethostbyname(hostname)
-    except Exception:
-        ip = "unknown"
-    return hostname, ip
+# def get_host_info():
+#     hostname = socket.gethostname()
+#     try:
+#         ip = socket.gethostbyname(hostname)
+#     except Exception:
+#         ip = "unknown"
+#     return hostname, ip
 
+
+# @app.get("/")
+# def root():
+#     hostname, ip = get_host_info()
+#     return {
+#         "status": "ok",
+#         "hostname": hostname,
+#         "ip": ip,
+#         "author": "Allex_DevOps"
+#     }
+
+
+# @app.get("/healthz")
+# def health():
+#     return {
+#         "status": "healthy"
+#     }
 
 @app.get("/")
 def root():
     hostname, ip = get_host_info()
+
+    now = datetime.now(timezone.utc)
+    uptime_seconds = int((now - START_TIME).total_seconds())
+
     return {
         "status": "ok",
         "hostname": hostname,
         "ip": ip,
-        "author": "Allex_DevOps"
+        "author": "Allex_DevOps",
+        "datetime": now.isoformat(),
+        "started_at": START_TIME.isoformat(),
+        "uptime_seconds": uptime_seconds
     }
 
 
